4390f0162a09332acf5c40956d0baed1808d1138
beanstown106 <nbenis106@gmail.com>
2015-10-24 23:10:16 +0200

    Allow to disable lockscreen media artwork
    abc: move the setting check to the custom observer
    and adapt code to latest android release
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: I08e1cb4666447787477f394004e5e66a0f8c739b
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -37,6 +37,7 @@ import android.media.session.MediaSession;
 import android.media.session.PlaybackState;
 import android.os.AsyncTask;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.NotificationStats;
 import android.service.notification.StatusBarNotification;
@@ -133,6 +134,7 @@ public class NotificationMediaManager implements Dumpable {
     private final DelayableExecutor mMainExecutor;
 
     private float mLockscreenMediaBlur;
+    private boolean mShowLockscreenMediaArt;
 
     private final Context mContext;
     private final ArrayList<MediaListener> mMediaListeners;
@@ -627,7 +629,7 @@ public class NotificationMediaManager implements Dumpable {
         }
 
         Bitmap artworkBitmap = null;
-        if (mediaMetadata != null && !mKeyguardBypassController.getBypassEnabled()) {
+        if (mShowLockscreenMediaArt && mediaMetadata != null && !mKeyguardBypassController.getBypassEnabled()) {
             artworkBitmap = mediaMetadata.getBitmap(MediaMetadata.METADATA_KEY_ART);
             if (artworkBitmap == null) {
                 artworkBitmap = mediaMetadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART);
@@ -854,6 +856,12 @@ public class NotificationMediaManager implements Dumpable {
         mLockscreenMediaBlur = (float) Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.LOCKSCREEN_MEDIA_BLUR, 25,
                 UserHandle.USER_CURRENT);
+    }
+
+    public void setLockScreenMediaArt() {
+        mShowLockscreenMediaArt = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SHOW_LOCKSCREEN_MEDIA_ART, 1,
+                UserHandle.USER_CURRENT) == 1;
     }
 
     @MainThread
