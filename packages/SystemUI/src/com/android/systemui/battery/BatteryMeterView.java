8c99fa10b3dc96d0ed1e22f7f5c3f7d889de1ff6
ezio84 <brabus84@gmail.com>
2018-02-14 00:24:18 +0100

    Battery Styles: Show a bolt ⚡ when charging
    Change-Id: I27f745e3458fda7c415a75d6e52f183e327cb498
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -283,8 +283,15 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         if (mBatteryPercentView == null) {
             return;
         }
-        mBatteryPercentView.setText(
-                NumberFormat.getPercentInstance().format(mLevel / 100f));
+
+        // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
+        // to load its emoji colored variant with the uFE0E flag
+        String bolt = "\u26A1\uFE0E";
+        CharSequence mChargeIndicator = mCharging && (mBatteryStyle == BATTERY_STYLE_TEXT)
+            ? (bolt + " ") : "";
+        mBatteryPercentView.setText(mChargeIndicator +
+            NumberFormat.getPercentInstance().format(mLevel / 100f));
+
         setContentDescription(
                 getContext().getString(mCharging ? R.string.accessibility_battery_level_charging
                         : R.string.accessibility_battery_level, mLevel));
@@ -331,6 +338,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
                 removeView(mBatteryPercentView);
                 mBatteryPercentView = null;
             }
+            updatePercentText();
         }
     }
 
