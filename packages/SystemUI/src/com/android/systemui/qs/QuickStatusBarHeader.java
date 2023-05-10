f42a370c83636333f4e625ac58ed913f48fd8183
Charsi-Tejas <tejas@m.ms.evolution-x.org>
2023-05-10 13:38:48 +0000

    SystemUI: Adjust qqs top margin dimension
    - This fixes the blank space between status bar and QS header after setting flag_combined_qs_headers to
     false.
    
    Change-Id: I85b70d71b4ca0ec99e03eb357bc73c1dad63e751
    Signed-off-by: Charsi-Tejas <tejas@m.ms.evolution-x.org>
    [adapted to pa]
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -385,15 +385,9 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         }
 
         MarginLayoutParams qqsLP = (MarginLayoutParams) mHeaderQsPanel.getLayoutParams();
-        if (largeScreenHeaderActive) {
-            qqsLP.topMargin = mContext.getResources()
-                    .getDimensionPixelSize(R.dimen.qqs_layout_margin_top);
-        } else if (!mUseCombinedQSHeader) {
-            qqsLP.topMargin = qsOffsetHeight;
-        } else {
-            qqsLP.topMargin = mContext.getResources()
-                    .getDimensionPixelSize(R.dimen.large_screen_shade_header_min_height);
-        }
+                qqsLP.topMargin = largeScreenHeaderActive || !mUseCombinedQSHeader
+                ? mContext.getResources().getDimensionPixelSize(R.dimen.qqs_layout_margin_top)
+                : qsOffsetHeight;
         mHeaderQsPanel.setLayoutParams(qqsLP);
 
         updateHeadersPadding();
