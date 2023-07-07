d47784e3936b4a63ff3696d06402dcb18f23413d
DennySPb <dennyspb@gmail.com>
2023-07-07 11:16:42 +0300

    Revert "SystemUI: Fix privacy indicator not showing for apps in parallel spaces"
    This reverts commit c381b12b50439f0cec6ad5cb4b9302ef2d4821bc.

--- packages/SystemUI/src/com/android/systemui/settings/UserTrackerImpl.kt
+++ packages/SystemUI/src/com/android/systemui/settings/UserTrackerImpl.kt
@@ -34,7 +34,6 @@ import androidx.annotation.WorkerThread
 import com.android.systemui.Dumpable
 import com.android.systemui.dump.DumpManager
 import com.android.systemui.util.Assert
-import ink.kaleidoscope.ParallelSpaceManager;
 import java.io.PrintWriter
 import java.lang.IllegalStateException
 import java.lang.ref.WeakReference
@@ -120,7 +119,6 @@ class UserTrackerImpl internal constructor(
             addAction(Intent.ACTION_MANAGED_PROFILE_ADDED)
             addAction(Intent.ACTION_MANAGED_PROFILE_REMOVED)
             addAction(Intent.ACTION_MANAGED_PROFILE_UNLOCKED)
-            addAction(Intent.ACTION_PARALLEL_SPACE_CHANGED)
         }
         context.registerReceiverForAllUsers(this, filter, null /* permission */, backgroundHandler)
 
@@ -136,8 +134,7 @@ class UserTrackerImpl internal constructor(
             Intent.ACTION_MANAGED_PROFILE_UNAVAILABLE,
             Intent.ACTION_MANAGED_PROFILE_ADDED,
             Intent.ACTION_MANAGED_PROFILE_REMOVED,
-            Intent.ACTION_MANAGED_PROFILE_UNLOCKED,
-            Intent.ACTION_PARALLEL_SPACE_CHANGED -> {
+            Intent.ACTION_MANAGED_PROFILE_UNLOCKED -> {
                 handleProfilesChanged()
             }
         }
@@ -151,7 +148,6 @@ class UserTrackerImpl internal constructor(
 
     private fun setUserIdInternal(user: Int): Pair<Context, List<UserInfo>> {
         val profiles = userManager.getProfiles(user)
-        profiles.addAll(ParallelSpaceManager.getInstance().getParallelUsers())
         val handle = UserHandle(user)
         val ctx = context.createContextAsUser(handle, 0)
 
@@ -222,7 +218,6 @@ class UserTrackerImpl internal constructor(
         Assert.isNotMainThread()
 
         val profiles = userManager.getProfiles(userId)
-        profiles.addAll(ParallelSpaceManager.getInstance().getParallelUsers())
         synchronized(mutex) {
             userProfiles = profiles.map { UserInfo(it) } // save a "deep" copy
         }
